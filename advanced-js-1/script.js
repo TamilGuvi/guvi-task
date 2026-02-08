8d96845c9215f86a22cc39aa60c1646ed691dd86
TAMILSELVAN-028 <ts.tamilselvan.1435@gmail.com>
2026-02-08 19:39:37 +0530

    JS-lp

---
+++ advanced-js-1/script.js
@@ -0,0 +1,34 @@
+// var data = document.getElementById("data");
+
+// const fetchUser = function(userName, callback) {
+//     setTimeout(function() {
+//         callback(userName);
+//     },2000);
+// };
+
+// const user = fetchUser("JOHN",function(tamil){
+//     data.innerHTML=tamil;
+// });                 
+
+// --------------------------------------------------------------------
+
+// let promiseObj = new Promise( function(resolve, reject) {
+//     console.log("Getting name form DB ...");
+//     setTimeout( function() {
+//         resolve("Tamil");
+//         // reject("Error occured");
+//     },2000);
+// });
+
+
+// promiseObj
+// .then(function(name) {
+//     console.log("The name is " +name)
+// })
+// .catch(function(error) {
+//     console.log("Alert!!! " +error)
+// })
+
+// --------------------------------------------------------------------
+
+
